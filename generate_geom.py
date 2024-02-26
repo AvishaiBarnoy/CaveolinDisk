6f2b320ed529c12ae83510f0f0ae729872fc80dc
avishai231 <avishai231@gmail.com>
2024-02-26 14:37:15 +0200

    better cyclic filter for small n

--- generate_geom.py
+++ generate_geom.py
@@ -9,23 +9,50 @@ from tqdm import tqdm
 def main(n_disks, r_disk, L):
     
     # Generate combinations
-    combinations = Combinatorics(n_disks)
-    raw_combinations = combinations.generate_combinations()
-    sys.stdout.write(f"Generated {len(raw_combinations)} combinations.\n")
+    if n_disks >= 12:
+        sys.stdout.write("""############
+# WARNING! #
+############
+This misses some combinations, e.g., for n=6 it misses [1,3,5]
+The other method is good up to n=11, since it is computationally heavy.\n
+""")
+        combinations = Combinatorics(n_disks)
+        raw_combinations = combinations.generate_combinations()
+        sys.stdout.write(f"Generated {len(raw_combinations)} combinations.\n")
     
-    # removed combinations not starting with first element
-    raw_combinations = [combination for combination in raw_combinations if combination[0] == 1] 
+        # removed combinations not starting with first element
+        raw_combinations = [combination for combination in raw_combinations if combination[0] == 1] 
+    
+        # remove combinations using cyclic filter
+        filtered_combinations = group_operations.cyclic_filter(raw_combinations, n_disks)
+        sys.stdout.write(f"Filtred redundant combinations, now we only have {len(filtered_combinations)} combinations.\n")
+    if n_disks < 12:
+        sys.stdout.write("""############
+# WARNING! #
+############
+This method does not filter mirror combinations [1,2,3] and [3,2,1] and only works up to n=11
+""")
+        # filter duplicate combinations 
+        filtered_combinations = set(group_operations.filter_partitions(n_disks)) 
+        sys.stdout.write(f"Filtred redundant combinations, now we only have {len(filtered_combinations)} combinations.\n")
+    
+    sys.stdout.write(f"""
+N proteins: {n_disks}
+protein radius: {r_disk} nm
+half-distance: {L} nm
+estimated caveolin radius {(2*r_disk+L)*n_disks / (2 * np.pi)} nm""")
 
-    # remove combinations using cyclic filter
-    filtered_combinations = group_operations.cyclic_filter(raw_combinations, n_disks)
-    sys.stdout.write(f"Filtred redundant combinations, now we only have {len(filtered_combinations)} combinations.\n")
-    
+
     # filtered_combinations = filtered_combinations[i_start:i_end]
     for i, combination in tqdm(enumerate(filtered_combinations), total=len(filtered_combinations)):
-        combination = Combination(combination, n_disks)
-        length_list = combination.map_combination_to_lengths()
-        mod_lengths = combination.modify_one_length(disk_radius=7, L=2)
+        if n_disks > 12:
+            combination = Combination(combination, n_disks)
+            length_list = combination.map_combination_to_lengths()
+        if n_disks < 12:
+            combination = Combination(lengths=combination, n_disks=n_disks)
         
+        mod_lengths = combination.modify_one_length(disk_radius=7, L=2)
+
         # check if combination is valid
         if combination.is_valid_inequality(L=2) == False:
             continue
